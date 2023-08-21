5477bd65ac72df6a2547e7729ff9a2f58467d416
Sumayya Kamran <105909253+Sumayya11@users.noreply.github.com>
2023-08-22 01:28:57 +0500

    flask server

---
+++ server/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify, render_template
+
+import util
+from util import classify_image
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/classify", methods=["POST"])
+def classify():
+    if request.method == "POST":
+        try:
+            image = request.files["image_data"]
+            if image:
+                img = image.read()
+                result = classify_image(img)  # Call your utility function
+                return jsonify({"result": result})
+            else:
+                return jsonify({"error": "No image uploaded."}), 400
+        except Exception as e:
+            return jsonify({"error": str(e)}), 500
+    elif request.method == "GET":
+        return jsonify({"message": "This is a GET request to the /classify endpoint."})
+    else:
+        return jsonify({"error": "Invalid request method."}), 400
+
+
+if __name__ == "__main__":
+    util.load_save_artifacts()
+    app.run(host="0.0.0.0", port=5000)
